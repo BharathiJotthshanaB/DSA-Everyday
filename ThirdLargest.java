f16205e28ef4c8bb5a3e70a378281bd6b473d6eb
BharathiJotthshanaB <jotthshana08@gmail.com>
2025-08-02 23:07:35 +0530

    solved third largest element in an array(duplicates allowed)[gfg]

---
+++ ThirdLargest.java
@@ -0,0 +1,23 @@
+class Solution {
+    int thirdLargest(int arr[]) {
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        int third = Integer.MIN_VALUE;
+                if (arr.length < 3) {
+            return -1; 
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > first) {
+                third = second;
+                second = first;
+                first = arr[i];
+            } else if (arr[i] > second) {
+                third = second;
+                second = arr[i];
+            } else if (arr[i] > third) {
+                third = arr[i];
+            }
+        }
+        return third;
+    }
+}
