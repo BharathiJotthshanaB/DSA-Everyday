fed34daa7d60e0d5153b16add9773f11a436f935
BharathiJotthshanaB <jotthshana08@gmail.com>
2025-08-03 20:07:46 +0530

    solved find the missing number in an array[gfg]

---
+++ missingNum.java
@@ -0,0 +1,15 @@
+class Solution{
+    int missingNum(int[]arr){
+        int n = arr.length+1;
+        int xor1 = 0;
+        int xor2 = 0;
+        for(int i=0;i<arr.length;i++){
+            xor1^=arr[i];
+            
+        }
+        for(int i=0;i<=n;i++){
+            xor2^=i;
+        }
+        return xor1^xor2;
+    }
+}
